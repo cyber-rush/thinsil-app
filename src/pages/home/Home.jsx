5037599a34ead8836a2b4b896cde6ecc2d99ee10
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2023-10-25 14:58:23 +0530

    ready to deploy

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,31 +6,20 @@ import Layout from "../../components/layout/Layout"
 import ProductCard from "../../components/productCard/ProductCard"
 import Testimonial from "../../components/testimonial/Testimonial"
 import Track from "../../components/track/Track"
-import { addToCart, deleteFromCart } from "../../redux/cartSlice"
+import { Link } from "react-router-dom"
 
 
 const Home = () => {
-    const dispatch = useDispatch()
-    const cartItem = useSelector(state => state.cart)
-
-    console.log(cartItem)
-    const addCart = () => {
-        dispatch(addToCart("shirt"))
-    }
-
-    const deleteCart = () => {
-        dispatch(deleteFromCart("shirt"))
-    }
-
     return (
         <Layout>
-            <div className="flex gap-5 justify-center">
-                <button className="bg-gray-300 p-5" onClick={addCart}>Add</button>
-                <button className="bg-gray-300 p-5" onClick={deleteCart}>Delete</button>
-            </div>
             <HeroSection />
             <Filter />
             <ProductCard />
+            <div className="flex justify-center -mt-10 mb-4">
+                <Link to={'/allproducts'}>
+                    <button className=' bg-gray-300 px-5 py-2 rounded-xl'>See more</button>
+                </Link>
+            </div>
             <Track />
             <Testimonial />
         </Layout>
